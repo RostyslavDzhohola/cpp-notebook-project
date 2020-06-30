c3d03186995904fbd51b420a93cb3fa515ad973c
Rostyslav <dg1h1t95@gmail.com>
2020-06-29 19:57:41 -0500

    New organized view
    This is a better-organized view

---
+++ Tasks.h
@@ -0,0 +1,21 @@
+#ifndef TASKS_H_INCLUDED
+#define TASKS_H_INCLUDED
+
+class TaskClass
+{
+private:
+    int m_taskId;                // Assign task ID number from 1 to infinity
+    char m_taskNote[50];
+    int m_priorityLevel;            // This will be the level from 1-3 by urgency and importance
+public:
+    void addTask();
+    void editTask();
+    void displayTask();
+    void readListOfTasks();
+    void completeTask();
+    int return_taskId();            // Removing task from the list of uncompleted tasks
+
+    TaskClass() = default;
+};
+
+#endif // TASKS_H_INCLUDED
---
+++ functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+void writeData();
+void displayData();
+void editTask(int id);
+void completeTask(int id);    // This functions takes ID number as a argument and deletes that task from note list
+
+#endif // FUNCTIONS_H_INCLUDED
